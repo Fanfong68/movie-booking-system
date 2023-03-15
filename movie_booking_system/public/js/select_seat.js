4b226ff759b187a24e24c05e8e05fe94eeddfa26
FanFong68 <fanfong68@gmail.com>
2023-03-15 10:29:03 +0800

    modify the select seat

---
+++ movie_booking_system/public/js/select_seat.js
@@ -0,0 +1,13 @@
+allSeats = document.querySelectorAll('.seat');
+
+for (var i = 0; i < allSeats.length; i++) {
+    var seat = allSeats[i];
+    seat.addEventListener('click', function () {
+        var bgclr = this.style.backgroundColor;
+        if(bgclr =='red')
+            this.style.backgroundColor = 'white'
+        else
+            this.style.backgroundColor = 'red'
+        debugger
+    }, false);
+}
--- movie_booking_system/resources/views/select_seat.blade.php
+++ movie_booking_system/resources/views/select_seat.blade.php
@@ -11,6 +11,43 @@
     </head>
 
     <body>
+        <div class="seat-container">
+            <table>
+                <tr>
+                    <td colspan="30"><div id="screen">Screen</div></td>
+                </tr>
+                <tr class="gap">
+                    <td colspan="30"></td>
+                </tr>
+                <?php for($j=0;$j<12;$j++) { 
+                    $z = 1;?>
+                <tr>
+                    <?php for($i=0;$i<5;$i++) { ?>
+                        <td class="row" value="<?=$j?>-<?=$z?>">
+                            <div class="seat"></div>
+                        </td>
+                    <?php $z++; } ?>
+
+                    <td class="walk"></td>
+
+                    <?php for($i=0;$i<15;$i++) { ?>
+                        <td class="row" value="<?=$j?>-<?=$z?>">
+                            <div class="seat"></div>
+                        </td>
+                    <?php $z++; } ?>
+
+                    <td class="walk"></td>
+
+                    <?php for($i=0;$i<5;$i++) { ?>
+                        <td class="row" value="<?=$j?>-<?=$z?>">
+                            <div class="seat"></div>
+                        </td>
+                    <?php $z++; } ?>
+                </tr>
+                <?php } ?>
+            </table>
+            <script src="js/select_seat.js"></script>
+        </div>
         <div class="page-container">
             <div id="proceed" class="proceed">
                 <div class="text">{{ __('Confirm') }}</div>
