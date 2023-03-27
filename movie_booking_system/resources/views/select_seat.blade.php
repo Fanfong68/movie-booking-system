cd0c931ce67c9447c1886b3780da3ffd77d1f5b5
FanFong68 <fanfong68@gmail.com>
2023-03-27 18:02:25 +0800

    modifeied select seat receipt

--- movie_booking_system/resources/views/select_seat.blade.php
+++ movie_booking_system/resources/views/select_seat.blade.php
@@ -58,11 +58,13 @@
                 </div>
                 <div class="receipt-body">
                     <h1>Receipt</h1>
-                    <p>Movie Title: </p>
-                    <p>Cinema     : </p>
-                    <p>Date       : </p>
-                    <p>Time       : </p>
-                    <p>Seat Number: </p>
+                    <p>Movie Title : </p>
+                    <p>Cinema : </p>
+                    <p>Date : </p>
+                    <p>Time : </p>
+                    <p>Seat Number : </p>
+                    <p>Ticket Count : </p>
+                    <h2>Total Price : </h2>
                 </div>
                 <div id="payment" class="payment w3-animate-bottom" onclick="loading()">
                     <div class="text">{{ __('Proceed Payment') }}</div>
