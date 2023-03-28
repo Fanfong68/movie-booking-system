aa0b55e955c71ff95842ce21ee6f9b31a020878b
YapKahKeat <yapkahkeat@hotmail.com>
2023-03-28 14:05:17 +0800

    create movie

--- movie_booking_system/resources/views/create_movie.blade.php
+++ movie_booking_system/resources/views/create_movie.blade.php
@@ -1,5 +1,10 @@
 <div>
     <form>
-        
+        @csrf
+        Movie Name:
+        <input type="text" id="movie" name="movie" value=''>
+        <br>
+        Poster:
+        <input type="file" name="poster" id="poster">
     </form>
 </div>
